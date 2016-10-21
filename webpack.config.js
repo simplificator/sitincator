8fc5e2f98fb8b154b09cb676f9e002983b31993a
Noëlle Rosenberg <noelle@Noelles-MacBook-Pro.local>
2016-10-21 22:27:19 +0200

    adding font and style settings

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,14 @@ module.exports = {
       },
       { 
         test: /\.scss$/,
-        loader: 'style-loader!css-loader!sass-loader'
+        loader: 'style-loader!css-loader!resolve-url-loader!sass-loader'
+      },
+      {
+        test: /\.(otf|eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
       }
     ]
-  }
+  },
+
+  devtool: 'cheap-module-eval-source-map'
 };
