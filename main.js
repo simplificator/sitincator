2e9084ca598d04c80fc923195577cc4415f108fa
Manuel Bouza <manuel.bouza@simplificator.com>
2016-10-22 11:21:14 +0200

    implement first version of calendar api

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
+var google = require('googleapis');
+const GCal = require('./src/gcal');
 
-let win
+
+let win;
+
+const gcal = new GCal();
+gcal.authorize().then(auth => {
+  var calendar = google.calendar('v3');
+  calendar.events.list({
+    auth: auth,
+    calendarId: 'primary',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime'
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var events = response.items;
+    if (events.length == 0) {
+      console.log('No upcoming events found.');
+    } else {
+      console.log('Upcoming 10 events:');
+      for (var i = 0; i < events.length; i++) {
+        var event = events[i];
+        var start = event.start.dateTime || event.start.date;
+        console.log('%s - %s', start, event.summary);
+      }
+    }
+  });
+}).catch(() => process.exit());
 
 function createWindow () {
   // Create the browser window.
@@ -18,6 +50,8 @@ function createWindow () {
     win = null;
   })
 }
+
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
---
+++ src/gcal/index.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const path = require('path');
+const rl = require('readline');
+const google = require('googleapis');
+const googleAuth = require('google-auth-library');
+
+const CREDENTIALS_DIR = path.resolve(__dirname, '../../credentials');
+const API_TOKEN = path.resolve(CREDENTIALS_DIR, 'token.json');
+const SITINCATOR_TOKEN = path.resolve(CREDENTIALS_DIR, 'sitincator.json');
+
+function readCredentials() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('config/client_secret.json', (err, content) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(JSON.parse(content));
+    });
+  });
+}
+
+function oauth2TokenInstructions(oauth2Client) {
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: ['https://www.googleapis.com/auth/calendar'],
+  });
+
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  console.log("\nStore the API Token in `credentials/sitincator.json`");
+}
+
+function storeToken(token) {
+  try {
+    fs.mkdirSync(CREDENTIALS_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(SITINCATOR_TOKEN, JSON.stringify(token));
+}
+
+function readOauth2Token(oauth2Client) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(SITINCATOR_TOKEN, (err, token) => {
+      if (err) {
+        fs.readFile(API_TOKEN, (err, code) => {
+          if (err) {
+            return reject(oauth2TokenInstructions(oauth2Client));
+          }
+          oauth2Client.getToken(JSON.parse(code), (err, token) => {
+            if (err) {
+              console.log('Error while trying to retrieve access token', err);
+              return reject(err);
+            }
+            storeToken(token);
+            resolve(token);
+          });
+        });
+      } else {
+        resolve(JSON.parse(token));
+      }
+    });
+  });
+}
+
+module.exports = class GCal {
+  constructor() {
+
+  }
+
+  authorize() {
+    return readCredentials().then(credentials => {
+      const clientSecret = credentials.installed.client_secret;
+      const clientId = credentials.installed.client_id;
+      const redirectUrl = credentials.installed.redirect_uris[0];
+      const auth = new googleAuth();
+      const oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+      return readOauth2Token(oauth2Client).then(token => {
+        oauth2Client.credentials = token;
+        return oauth2Client;
+      });
+    });
+  }
+}
