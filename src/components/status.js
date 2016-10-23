41520570f8d1c886f4ff4045f5c471a3983eaebd
Manuel Bouza <manuel.bouza@simplificator.com>
2016-10-23 11:59:46 +0200

    animate transition between booked and free

--- src/components/status.js
+++ src/components/status.js
@@ -3,6 +3,7 @@ import Button from './button';
 import { ipcRenderer } from 'electron';
 import EventDetails from './event_details';
 import classNames from 'classnames';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 export default class Status extends Component {
 
@@ -71,9 +72,16 @@ export default class Status extends Component {
       'booked': this.isBooked(),
     });
 
+    const innerComponents = this.isBooked() ? [null, this.renderBooked()] : [this.renderFree(), null];
+
     return (
       <div className={rootClasses}>
-        {this.isBooked() ? this.renderBooked() : this.renderFree()}
+        <ReactCSSTransitionGroup
+          transitionName="fade"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}>
+          {innerComponents}
+        </ReactCSSTransitionGroup>
         <EventDetails
           displayedEvent={this.state.displayedEvent}
           expanded={this.state.detailsExpanded}
@@ -86,9 +94,9 @@ export default class Status extends Component {
 
   renderFree() {
     return (
-      <div className='status-details'>
+      <div className='status-details' key={0}>
         <h3>Quick Booking</h3>
-        <div className="action-buttons">
+        <div className="action-buttons multiple">
           <Button icon="15-min" handleClick={this.handleQuickReservation.bind(this, 15)}/>
           <Button icon="30-min" handleClick={this.handleQuickReservation.bind(this, 30)}/>
         </div>
@@ -99,8 +107,9 @@ export default class Status extends Component {
 
   renderBooked() {
     return (
-      <div className='status-details'>
-        <div className="action-buttons">
+      <div className='status-details' key={1}>
+        <h3> </h3>
+        <div className="action-buttons single">
           <Button icon="cancel" className="big" handleClick={this.handleFinishReservation.bind(this)}/>
         </div>
         <h1>Booked</h1>
